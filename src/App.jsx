22aebf15a08932c1a9754dbe984de5694f962ff2
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-03-26 15:26:54 +0100

    add form to submit new blog titles and update title list dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,50 @@ const blogTitles = [
 
 export default function App() {
 
+  const [newTitle, setNewTitle] = useState('')
+  const [titles, setTitles] = useState(blogTitles)
+
+  function handleFormSubmit(e) {
+    e.preventDefault()
+    console.log(newTitle);
+
+    setTitles([...titles, newTitle])
+
+    console.log(titles);
+
+    setNewTitle('')
+  }
 
   return (
-    <div className="container">
-      <ul className="list-group">
-        {blogTitles.map((title, index) => (
-          <li className="list-group-item d-flex justify-content-between align-items-center"
-            key={index}
-          >{title}</li>
-        ))}
-      </ul>
-    </div>
+
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <div className="mb-3">
+          <label htmlFor="" className="form-label">Titolo</label>
+          <input
+            type="text"
+            className="form-control"
+            name="title"
+            id="title"
+            aria-describedby="helpId"
+            placeholder="inserisci il titolo"
+            value={newTitle}
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+          <small id="helpId" className="form-text text-muted">inserisci un titolo per il blog</small>
+        </div>
+      </form>
+
+      <div className="container">
+        <ul className="list-group">
+          {blogTitles.map((title, index) => (
+            <li className="list-group-item d-flex justify-content-between align-items-center"
+              key={index}
+            >{title}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+
   )
 }
